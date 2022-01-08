b290948eb70ca1fd240b921181ec976c8793d7b3
ramram419 <barbara0419@naver.com>
2022-01-08 14:56:04 +0900

    first commit

---
+++ src/main/java/com/example/demo/FrontController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FrontController {
+	
+	@RequestMapping(value = "/")
+	public String Main() {
+		return "index";
+	}
+}
